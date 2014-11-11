88c268cc2fb9f5057daf779b128eddd30acdd6b3
Antoine Ponsard <aponsard@cs.ubc.ca>
2014-11-10 19:38:59 -0800

    Refactor main.js and fringeView
    TODO: create namespace for constant parameters

--- d3/fringeView.js
+++ d3/fringeView.js
@@ -1,87 +1,5 @@
-///////////////////   Parameters   /////////////////
-
-var paperMinRadius = 5,
-    paperMaxRadius = 15,
-    paperInnerWhiteCircleRatio =.4,
-    paperOutlineWidth = 4,	// UNUSED - this divided by 2 must be > min radius
-    paperMarginBottom = 5,
-    titleBaselineOffset = 6,  // depends on font size
-    titleXOffset = 5,
-    paperXOffsetWhenSelected = - (2*paperMaxRadius - titleXOffset);
-
-// Defines the dimension of each region, index by the current view (core, toread, fringe)
-// The apparent width is the horizontal space that we want the region to occupy on the screen
-// An appropriate offset for the x-position of the center will be computed as -radius+apparentWidth
-var coreRadius = [120,120,120],
-    toreadRadius = [2000,2000,2000],
-    fringeRadius = [2000,2000,2000],
-    coreApparentWidth = [120,120,120],
-    fringeApparentWidth = [420,420,420],
-    toreadApparentWidth = [420,420,fringeApparentWidth[2]-paperMaxRadius+titleXOffset];
-
-var colors={
-	"blue":"#00A1CB",
-	"green":"#61AE24",
-	"pink":"#D70060",
-	"orange":"#F18D05",
-	"darkblue":"#113F8C",
-	"turquoise":"#01A4A4",	    // This color and all the following are not to be used for the nodes
-	"red":"#E54028",	
-	"darkgray":"#616161",	
-    "toread":"rgb(242, 222, 195)", 
-    "toreadBorder":"rgb(242, 210, 166)",
-    "core":"rgb(223, 111, 95)"
-}
-
-var currentYear=2010;
-
-
-////////////////    Global variables    //////////////
-
-var view=2; // 0=core, 1=to read, 2=fringe
-
-var userData={
-    "core":[
-    {
-        // Triggers and barriers to customization
-        "doi":"10.1145/108844.108867" },
-    {
-        // Buttons
-        "doi":"10.1145/97243.97271" },
-    {
-        // Medium vs Mechanism
-        "doi":"10.1007/978-94-011-0349-7_9" }
-    ]}; 
-
-////////////////    Load previous session    //////////////
-
-// Handle local storage of objects
-Storage.prototype.setObject = function(key, value) {
-    this.setItem(key, JSON.stringify(value));
-}
-
-Storage.prototype.getObject = function(key) {
-    var value = this.getItem(key);
-    return value && JSON.parse(value);
-}
-
-function saveSession(){
-    localStorage.setObject("userData", userData);
-    console.log("session saved in localStorage('userData')");
-}
-
-var retrievedData=localStorage.getObject("userData");
-console.log(retrievedData);
-
-////////////////	   Main rendering       //////////////
-
-// I'm not sure what was the point of .select("body").append("svg") instead of select("svg")...
-var svg = d3.select("body").append("svg")
-            .attr("width",window.innerWidth)
-            .attr("height",window.innerHeight);
-
 // Build the components of the vis, in the appropriate z-index order
-d3.tsv("data/SmallDataset.tsv", function(data){
+function createVis(data){
     // toread
     svg.append("circle")
         .attr("id","toread")
@@ -110,14 +28,10 @@ d3.tsv("data/SmallDataset.tsv", function(data){
     papers.append("text")
         .attr("class", "title")
         .text(function(d,i) {return d.title;} )
-
-    // Initialize interaction & visual appearance
-    //createVis();
-    drawVis();
-    bindListeners();
-});
+}
 
 
+// Specify positions and styles
 function drawVis(){
     // fringe
     d3.selectAll(".node")
@@ -160,6 +74,7 @@ function drawVis(){
         .attr("fill",colors.darkgray);*/
 }
 
+
 // Specify interaction
 function bindListeners(){
     
@@ -200,37 +115,30 @@ function bindListeners(){
     })
 }
 
-// Dynamic resize
-window.onresize = function(){
-    svg.attr("width", window.innerWidth)
-       .attr("height", window.innerHeight);
-    drawVis();
-}
 
-
-////////////////	Helper functions    //////////////
+////////////////    Helper functions    //////////////
 
 // Compute X coordinate for the i-th paper on the fringe, based on a circle
 function fringePaperX(i){
-	var h=window.innerHeight;
+    var h=window.innerHeight;
     var xoffset=-fringeRadius[view]+fringeApparentWidth[view];
-	return xoffset+Math.sqrt(Math.pow(fringeRadius[view],2)-Math.pow(h/2-fringePaperY(i),2))+paperMaxRadius;
+    return xoffset+Math.sqrt(Math.pow(fringeRadius[view],2)-Math.pow(h/2-fringePaperY(i),2))+paperMaxRadius;
 }
 
 // Compute Y coordinate for the i-th paper on the fringe
 function fringePaperY(i){
-	return paperMaxRadius+i*(2*paperMaxRadius+paperMarginBottom);
+    return paperMaxRadius+i*(2*paperMaxRadius+paperMarginBottom);
 }
 
 // Compute a node radius from the value supplied, between min and max
 // If we want to display an outline instead of a fill circle, the radius must be smaller
 function radius(value){
-	return Math.max(paperMinRadius, Math.min(paperMaxRadius,
-		currentYear-value));
+    return Math.max(paperMinRadius, Math.min(paperMaxRadius,
+        currentYear-value));
 }
 
 // Return a random color except red or turquoise
 function randomColor(){
-	var keys=Object.keys(colors);
-	return colors[keys[ (keys.length-6) * Math.random() << 0]];
+    var keys=Object.keys(colors);
+    return colors[keys[ (keys.length-6) * Math.random() << 0]];
 }
