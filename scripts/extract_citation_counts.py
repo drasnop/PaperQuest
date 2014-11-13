28dd9921764bc71667b9fd00f255f8baf2903bf9
pax <pax@cs.ubc.ca>
2014-11-12 19:38:16 -0800

    First versions of scripts to parse data from CHI and UIST, and add citations counts from Google.
    The citation counts come from a KimonoLabs crawl of Google Search,
    using the DOIs for the papers we're looking for.  The script scans the
    results looking for citation counts and associated DOIs.  We also get
    the publication title so that we can do some validation later with the
    original dataset.
    
    The other script just changes the format of the dataset, from a file
    of values separated by tabs (tsv) to a JSON format.  The script also
    builds the "referenced by" relationships, using the "cited by"
    relationship and searching for the specified publications in the
    dataset.

---
+++ scripts/extract_citation_counts.py
@@ -0,0 +1,51 @@
+################################################################################
+#
+# Extracts citation counts from Google Search results, looking for
+# specific patterns within the results. The scripts assumes it
+# receives as input a file with a JSON array containing all the
+# results, such as those returned by KimonoLabs. All results should be
+# in the 'collection1', and they should all be called 'result'.
+
+import json
+import re
+import sys
+
+
+citationPattern = re.compile('(.*)Cited by (\d+).*doi>(.*)')
+# For our purposes, assume DOIs don't contain spaces. The rest of the
+# regex is meant to help clean the DOI from extra stuff that might
+# have gotten attached to it in the initial pattern parsing.
+doiPattern = re.compile(' *([^ ]+)[\] \xa0].*')
+
+
+def extractDOI(s):
+    return doiPattern.findall(s)[0].strip('], ')
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print "You have to specify the file with the Kimono results"
+        exit (1);
+
+    results = None
+
+    # Parse the contents of the input file into a Python
+    # object. They're expected to be in JSON, as output by Kimono
+    with open(sys.argv[1]) as input_file:
+        results = json.loads(input_file.read())
+
+    # Get rid of extra elements, keep just the actual results
+    results = [r['result'] for r in results['results']['collection1']]
+
+    # Filter out results that don't seem to contain citation count
+    # information. We use regular expressions to search for both a
+    # "Cited by" string and a "doi>" string.
+    results = [citationPattern.findall(r['text'])[0]
+               for r in results
+               if citationPattern.search(r['text'])]
+
+    counts = {}
+    for r in results:
+        counts[extractDOI(r[2])] = dict(title=r[0],
+                                        citation_count=int(r[1]))
+
+    print counts
