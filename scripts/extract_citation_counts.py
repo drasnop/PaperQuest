636d8080e734a0803f8c97b27e5b3754e29feece
pax <pax@cs.ubc.ca>
2014-11-13 18:28:32 -0800

    Changes script to extract citation counts to be more accurate.
    The script now uses the URL associated with each result to determine
    the DOI to which the result refers.  Before we were scanning the text
    of the result for a DOI, but this often gave us a false match because
    the ACM page includes DOIs for many different papers (citations and
    references).

--- scripts/extract_citation_counts.py
+++ scripts/extract_citation_counts.py
@@ -11,17 +11,12 @@ import re
 import sys
 
 
-citationPattern = re.compile('(.*)Cited by (\d+).*doi>([0-9\.]+/[0-9\.]+[0-9])')
-# For our purposes, assume DOIs consist of numbers, dots and
-# slashes. All the DOIs we're interested in match this pattern. The
-# rest of the regex is meant to help clean the DOI from extra stuff
-# that might have gotten attached to it in the initial pattern
-# parsing.
-doiPattern = re.compile(' *([0-9\.]+/[0-9\.]+[0-9]).*?')
-
+citationPattern = re.compile('(.*)Cited by (\d+).*')
+# Extracts the DOI suffix of a publication from a dl.acm.org URL
+doiACMSuffixPattern = re.compile('id=(\d+\.\d+|\d+)')
 
 def extractDOI(s):
-    return doiPattern.findall(s)[0].strip('], ')
+    return doiACMSuffixPattern.findall(s)[0]
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -35,19 +30,20 @@ if __name__ == "__main__":
     with open(sys.argv[1]) as input_file:
         results = json.loads(input_file.read())
 
-    # Get rid of extra elements, keep just the actual results
-    results = [r['result'] for r in results['results']['collection1']]
-
-    # Filter out results that don't seem to contain citation count
-    # information. We use regular expressions to search for both a
-    # "Cited by" string and a "doi>" string.
-    results = [citationPattern.findall(r['text'])[0]
-               for r in results
-               if citationPattern.search(r['text'])]
+    # Get rid of extra elements, keep just the actual results. Keep
+    # only those that have a URL pointing to the ACM digital
+    # library. Filter out results that don't seem to contain citation
+    # count information. We use regular expressions to search for a
+    # "Cited by" string.
+    results = [r['result']
+               for r in results['results']['collection1']
+               if (r['result']['href'].find('dl.acm') >= 0 and
+                   citationPattern.search(r['result']['text']))]
 
     counts = {}
     for r in results:
-        counts[extractDOI(r[2])] = dict(title=r[0],
-                                        citation_count=int(r[1]))
+        tmp = citationPattern.findall(r['text'])[0]
+        counts[extractDOI(r['href'])] = dict(title=tmp[0],
+                                             citation_count=int(tmp[1]))
 
     print json.dumps(counts)
