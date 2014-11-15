e2a378bdc530009710ac039f8bf3dd8c20bcbf47
Antoine Ponsard <aponsard@cs.ubc.ca>
2014-11-14 17:19:34 -0800

    draft of the algorithm pseudo code

--- js/algorithm.js
+++ js/algorithm.js
@@ -4,4 +4,34 @@
 
 function computeFringe(){
 	userData.fringe=global.papers.slice(0,100);
+
+
+/*
+pseudo-code
+
+// generate first fringe
+
+for each paper P in core, toRead, fringe
+	for each reference R in P
+		 if R is not in core or toRead 
+		 	if fringe doesn't contain R
+				add R to fringe
+				set R.relevance=ACC
+			R.relevance+=w
+	for each citation C in P
+		idem
+
+// when changing the state of one paper P either in core, toRead or fringe
+
+for each reference R in P
+	 if R is not in core or toRead 
+	 	// R has to be in the fringe, if the fringe has been initialized.
+	 	// but maybe we shouldn't initiliaze the fringe, and instead call update on each of the seed papers
+		R.relevance+= w' - w (add new weight, remove previous one)
+for each citation C in P
+	idem
+
+
+
+*/	
 }
