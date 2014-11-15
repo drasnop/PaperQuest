12a341cbe23d2337b267d2c650dc2cf45d1c09ef
Antoine Ponsard <aponsard@cs.ubc.ca>
2014-11-14 17:35:57 -0800

    second draft (still lots of room for improvement!)

--- js/algorithm.js
+++ js/algorithm.js
@@ -9,29 +9,35 @@ function computeFringe(){
 /*
 pseudo-code
 
-// generate first fringe
+// all the papers that matter to the user
+knownPapers = core + toRead + fringe (which contains selected)
 
-for each paper P in core, toRead, fringe
+// all the papers that impact the computation of the relevance score
+interestSet = core + toRead + selected
+
+// call this function everytime a paper is added to the interest set
+function updateRelevanceScoresWhenInserting(P)
 	for each reference R in P
-		 if R is not in core or toRead 
-		 	if fringe doesn't contain R
-				add R to fringe
-				set R.relevance=ACC
-			R.relevance+=w
+		if knownPapers doesn't contain R
+			add R to knownPapers
+			set R.relevance=ACC
+		R.relevance+=w[P.type]
 	for each citation C in P
 		idem
 
-// when changing the state of one paper P either in core, toRead or fringe
+// call this function everytime a paper moves inside the interest set
+function updateRelevanceScoresWhenMoving(P)
+	for each reference R in P
+		// in theory all its references and citations are already known
+		// Maybe there is no performance gain in separating these two update functions,
+		// if checking for existance of R in knownPapers has the same cost as doing knowPapers.R.relevance
+		R.relevance+=w[P.type]
+	for each citation C in P
+		idem
 
-for each reference R in P
-	 if R is not in core or toRead 
-	 	// R has to be in the fringe, if the fringe has been initialized.
-	 	// but maybe we shouldn't initiliaze the fringe, and instead call update on each of the seed papers
-		R.relevance+= w' - w (add new weight, remove previous one)
-for each citation C in P
-	idem
-
-
+// generate first fringe
+for each paper P in core, toRead, fringe
+	updateRelevanceScoresWhenInserting(P)
 
 */	
 }
